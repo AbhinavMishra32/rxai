03682d5bace772b483576d141d8061136e042a28
rapidtyper <abhinavmishra3322@gmail.com>
2024-07-01 22:36:43 +0530

    chore: Update TodoCard and TodoInput components with improved styling and functionality

--- todoapp/src/TodoCard.tsx
+++ todoapp/src/TodoCard.tsx
@@ -7,12 +7,11 @@ export default function TodoCard(props) {
     return (
         <li key={props.todoIndex}>
             {children}
-            <div className="bg-stone-800 m-4 border border-stone-600 p-3 rounded-2xl flex items-center bg-gradient-to-br from-stone-900 to-stone-800 hover:scale-105 transition-all ease-in-out hover:shadow-xl hover:shadow-stone-600/40">
-                <input type="checkbox" className='relative peer shrink-0 
-  appearance-none w-6 h-6 border-2 border-stone-500 rounded-md bg-stone-600 focus:ring-offset-stone-600 focus:ring-2  hover:bg-stone-500  hover:border-stone-700 focus:ring-stone-300 transition-all ease-in-out checked:bg-stone-300 '
+            <div className="bg-stone-800 m-3 border border-stone-600 p-3 rounded-2xl flex items-center bg-gradient-to-br from-stone-900 to-stone-800 transition-all ease-in-out">
+                <input type="checkbox" className='relative peer shrink-0 appearance-none w-6 h-6 border-2 border-stone-500 rounded-md bg-stone-600 focus:ring-offset-stone-600 focus:ring-2  hover:bg-stone-500  hover:border-stone-700 focus:ring-stone-300 transition-all ease-in-out checked:bg-stone-300 '
                     onClick={() => { handleDeleteTodos(index) }} />
-                <p className="ml-4 font-unbounded text-stone-300">{props.todos}</p>
-                <div className="ml-auto">
+                <p className="ml-4 font-unbounded text-stone-300 truncate">{props.todos}</p>
+                <div className="ml-auto flex items-center">
                     <button onClick={() => { handleEditTodo(index) }}>
                         <FontAwesomeIcon icon={faPenToSquare} className="bg-stone-500/40 p-2 flex rounded-lg border border-stone-300/30 mx-3 text-stone-300  hover:text-red-800 hover:shadow-xl hover:shadow-stone-600/40 transition-all ease-in-out" />
                     </button>
