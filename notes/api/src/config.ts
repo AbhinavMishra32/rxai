308e13e62c8b0bb73102950475258321dfb87491
Abhinav Mishra <abhinavmishra3322@gmail.com>
2024-10-17 22:41:35 +0530

    Add Prisma schema for User and Note models, update config for JWT secrets, and include .gitignore and workspace configuration

--- notes/api/src/config.ts
+++ notes/api/src/config.ts
@@ -2,11 +2,12 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const JWT_SECRET = process.env.JWT_SECRET;
-const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+// if implementing own authentication:
+// const JWT_SECRET = process.env.JWT_SECRET;
+// const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
-if (!JWT_SECRET || !REFRESH_TOKEN_SECRET) {
-    throw new Error("JWT_SECRET or REFRESH_TOKEN_SECRET is not defined");
-}
+// if (!JWT_SECRET || !REFRESH_TOKEN_SECRET) {
+//     throw new Error("JWT_SECRET or REFRESH_TOKEN_SECRET is not defined");
+// }
 
-export { JWT_SECRET, REFRESH_TOKEN_SECRET };
+// export { JWT_SECRET, REFRESH_TOKEN_SECRET };
