df915fb5927247dc6ec237bcff70fc45dfb8e6c2
rapidtyper <abhinavmishra3322@gmail.com>
2024-10-30 16:40:12 +0530

    Refactor SignInPage layout for improved responsiveness and styling

--- notes/client/src/routes/SignInPage.tsx
+++ notes/client/src/routes/SignInPage.tsx
@@ -16,23 +16,27 @@ const SignInPage = () => {
     })
     return (
         <>
-            <div className="flex items-center h-screen justify-center overflow-hidden bg-gradient-to-b from-indigo-700/20 to-black">
+            <div className="flex items-center min-h-screen justify-center overflow-hidden bg-gradient-to-b from-indigo-700/20 to-black">
                 <div className="flex flex-col xl:flex-row justify-center items-center gap-6 w-11/12 lg:w-[90vw] lg:max-w-[60vw] lg:h-4/6 overflow-hidden lg:bg-black rounded-3xl shadow-lg p-8">
 
-                    <div className="flex w-full lg:w-full justify-center items-center lg:rounded-xl">
-                        <div className='mt-10'>
+                    <div className="flex w-full lg:w-1/2 justify-center items-center lg:rounded-xl">
+                        <div className='lg:mt-10'>
                             <SignedOut>
                                 <SignIn path="/sign-in" afterSignOutUrl="/app/home" signUpUrl="/sign-up" />
                             </SignedOut>
                         </div>
                     </div>
 
-                    <div className="relative flex justify-center items-center w-full lg:w-full h-[300px] lg:h-full lg:max-h-[60vh] lg:rounded-xl overflow-hidden">
+                    <div className="relative flex lg:translate-y-0 -translate-y-14 justify-center items-center w-full lg:w-1/2 h-[300px] lg:h-full lg:max-h-[60vh] lg:rounded-xl overflow-hidden">
                         <Spline scene="https://prod.spline.design/sqFJBN7iWvh7fJtc/scene.splinecode" />
                     </div>
                 </div>
             </div>
-
+            <style>{`
+            body {
+                overflow: hidden;
+            }
+            `}</style>
         </>
 
     )
